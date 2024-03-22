9b022ef99ee0ce1922601d7c967bf9b1ae5972d8
EtoAndruwa <timonina1909@gmail.com>
2024-03-23 00:13:19 +0300

    working on wraper of the emitc.div function at the moment

--- mlir/lib/Dialect/EmitC/Transforms/DivToOuterFunc.cpp
+++ mlir/lib/Dialect/EmitC/Transforms/DivToOuterFunc.cpp
@@ -23,72 +23,94 @@ namespace
   {
     void runOnOperation() override 
     {
-      ++pass_called_num;
-
+      // ++pass_called_num;
       Operation* rootOp = getOperation();
-      auto module = castToModule(rootOp);
+      OpBuilder builder(&getContext());
 
-      if (module) 
-      {
-        // If the current operation is not a module, report an error or handle appropriately.
-        
-        OpBuilder builder(&getContext());
-
-        // Define the integer type (e.g., i32)
-        IntegerType intType = IntegerType::get(&getContext(), 32); // 32-bit integer type
-
-        // Create a constant integer with value 42
-        Value constantInt = builder.create<ConstantOp>(builder.getUnknownLoc(), intType,
-                                                      builder.getIntegerAttr(intType, 42));
-
-        // Print the MLIR IR of the constant integer value
-        constantInt.dump();
-        module.push_back(constantInt.getDefiningOp());                  
-      }
-
-      // print_all_ops(rootOp);
-
-      check_operation(rootOp);
-
+      check_operation(rootOp, builder, module);
       print_debug_data();
     }
 
     // private methods section
     private:
       
-      // casts the root operation to the module
-      ModuleOp castToModule(Operation *op) 
-      {
-        return dyn_cast<ModuleOp>(op);
-      }     
 
       // checks the operation
-      void check_operation(Operation *op)
+      void check_operation(Operation *op, OpBuilder& builder, ModuleOp& module, Region& region, Block& block)
       {
         llvm::StringRef div_strref = "emitc.div";
         auto cur_op_name = op->getName().getStringRef();
-        
+
+        if (region->getBlocks().size() == 1)
+        {
+
+        }
+
         if (cur_op_name == div_strref)
         {
+          create_div_wrapper(builder, module);
+          // auto intType = builder.getIntegerType(32);
+          // SmallVector<Type, 2> inputTypes({intType, intType});
+          // SmallVector<Type, 1> resultTypes({intType});
+
+          // // Create operands for input values
+          // Value operand1 = builder.create<emitc::ConstantOp>(builder.getUnknownLoc(), intType, builder.getIntegerAttr(intType, 10));
+          // Value operand2 = builder.create<emitc::ConstantOp>(builder.getUnknownLoc(), intType, builder.getIntegerAttr(intType, 20));
+
+          // Create an EmitC call operation and add it to the module
+          auto calleeAttr = builder.getStringAttr("foo_0");
+          OperationState state(builder.getUnknownLoc(), "emitc.call");
+          state.addAttribute("callee", calleeAttr);
+          Operation *callOp = Operation::create(state);
+          block.push_back(callOp);
+
+
+          // std::string cur_func_name = "foo_" + std::to_string(cur_func_num);
+
+     
+          // OperationState state(builder.getUnknownLoc(), "emitc.call");
+          // state.addAttribute("callee", builder.getStringAttr(cur_func_name));
+
+          // // Add other attributes, operands, and results as needed
+
+          // Operation *callOp = Operation::create(state);
+          // module.push_back(callOp);
           ++cur_func_num;
         }
 
-        for (Region &region : op->getRegions())
-          check_regions(region);
+        for (Region &region: op->getRegions())
+        {
+          check_regions(region, builder, module);
+        }
+      }
+
+      void create_div_wrapper(OpBuilder& builder, ModuleOp& module, Region& region, Block& block)
+      {
+        Location loc = builder.getUnknownLoc();
+        auto funcType = builder.getFunctionType({}, {});
+
+        OperationState state(loc, "emitc.func");
+        state.addAttribute("sym_name", builder.getStringAttr("foo_0"));
+        state.addAttribute("type", TypeAttr::get(funcType));
+        Operation *funcOp = Operation::create(state);
+
+        Region &region = module.getBodyRegion();
+        Block &block = region.getBodyBlock();
+        block.push_back(funcOp);
       }
 
       // iterates on all blocks in the region
-      void check_regions(Region& region)
+      void check_regions(Region& region, OpBuilder& builder, ModuleOp& module, Region& region, Block& block)
       {
-        for (Block &block : region.getBlocks())
-          check_blocks(block);
+        for (Block &block: region.getBlocks())
+          check_blocks(block, builder, module, region, block);
       }
 
       // iterates on all operation in the block
-      void check_blocks(Block &block)
+      void check_blocks(Block &block, OpBuilder& builder, ModuleOp& module, Region& region, Block& block)
       {
-        for (Operation &op : block.getOperations())
-          check_operation(&op);
+        for (Operation &op: block.getOperations())
+          check_operation(&op, builder, module, region, block);
       }
       
       void getDependentDialects(DialectRegistry &registry) const override 
@@ -103,18 +125,24 @@ namespace
 
         outs << "\n=================================DEBUG=================================\n";
         outs << "The total number of created funcs = " << cur_func_num << "\n";
-        outs << "The total number of pass calls = " << pass_called_num << "\n";
+        // outs << "The total number of pass calls = " << pass_called_num << "\n";
         outs << "=================================DEBUG=================================\n";
       }
+
+      // casts the root operation to the module
+      ModuleOp castToModule(Operation *op) 
+      {
+        return dyn_cast<ModuleOp>(op);
+      }     
 
     // private variables section
     private:
       static size_t cur_func_num;    // stores total number of created function in order to create lables
-      static size_t pass_called_num; // stores the total number of times when pass called
+      // static size_t pass_called_num; // stores the total number of times when pass called
   }; 
 
 size_t DivToOuterFuncPass::cur_func_num = 0;    // stores total number of created function in order to create lables
-size_t DivToOuterFuncPass::pass_called_num = 0; // stores the total number of times when pass called
+// size_t DivToOuterFuncPass::pass_called_num = 0; // stores the total number of times when pass called
 
 } // namespace
 
