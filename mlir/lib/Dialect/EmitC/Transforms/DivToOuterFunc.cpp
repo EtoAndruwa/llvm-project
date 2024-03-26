e25ef9ad303d737b40fd0c047cee7d46220c86f7
EtoAndruwa <timonina1909@gmail.com>
2024-03-26 19:53:47 +0300

    Working on the documentation for the pass

--- mlir/lib/Dialect/EmitC/Transforms/DivToOuterFunc.cpp
+++ mlir/lib/Dialect/EmitC/Transforms/DivToOuterFunc.cpp
@@ -16,131 +16,136 @@ namespace mlir
 using namespace mlir;
 using namespace emitc;
 
-namespace 
+namespace // <--------- namespace's start
 {
   // this is struct for my pass 
   struct DivToOuterFuncPass: public emitc::impl::DivToOuterFuncBase<DivToOuterFuncPass> 
   {
-    void runOnOperation() override 
-    {
-      MLIRContext* context = &getContext();
-      Operation* rootOp = getOperation();
-      module_ptr = rootOp; // saving module ptr in the class's field
-
-      reg_ptr = &(module_ptr->getRegion(0));
-
-      check_operation(context, nullptr, nullptr, rootOp);
-    }
-
+    // public methods/variables section 
     public:
-      typedef enum class op_types
+
+      /**
+       * Entry function for the transformation 
+       * using this operation pass by MLIR.
+      */
+      void runOnOperation() override 
+      {
+        MLIRContext* context_ptr = &getContext();   // gets the context 
+        Operation* rootOp        = getOperation();  // gets the pointer to the top level operation in the context
+        module_ptr               = rootOp;          // saves module ptr in the class's field for the future creation of wrapping function
+        check_operation(context_ptr, nullptr, nullptr, rootOp);
+      }
+
+      /**
+       * This enum is used for operands/returns types. 
+      */
+      typedef enum class operand_types
       {
         INT,
         FLOAT,
         OPAQUE,
-      }op_types;
-
-      // typedef enum class error_code
-      // {
-      //   OPAQUE_VAL = -1,
-      // }error_code;
+      }operand_types;
 
     // private methods section
     private:
     
-      // checks the operation
-      bool check_operation(MLIRContext* context, Region* region, Block* block, Operation* op)
+      /**
+       * This method checks the operation for being "emitc.div" operation.
+       * If so, it calls the method 'transform'. Otherwise enters nested regions/blocks.
+      */
+      bool check_operation(MLIRContext* context_ptr, Region* region_ptr, Block* block_ptr, Operation* operation_ptr)
       {
-        if (op == nullptr)
+        if (operation_ptr == nullptr)
         {
-          return false;
-        }
-
-        static llvm::StringRef div_strref = "emitc.div";
-        auto cur_op_name = op->getName().getStringRef();
-
-        if (cur_op_name == div_strref)
-        {
-          Region& reg = module_ptr->getRegion(0); // getting the first region in the module
-          OpBuilder wrapper_builder(reg); // settign the builder for that region (ALWAYS!!! to the start of the region)
-
-          std::vector<Type> return_types  = getReturnTypes(op); // getting string like 'i32'
-          std::vector<Type> operand_types = getOperandTypes(op); // getting string like 'i32'
-
-          SmallVector<Type> argTypes; // for types built by builder
-          SmallVector<Type> retTypes; // for types built by builder
-          build_op_types(argTypes, operand_types, wrapper_builder); // setting types of args from strings
-          build_op_types(retTypes, return_types, wrapper_builder); // setting types of ret from strings
-
-          FuncOp funcOp = create_div_wrapper(context, wrapper_builder, argTypes, retTypes); // creates the emitc.div wrapper
-
-          OpBuilder callOp_builder(op);
-          Operation* callOp = callOp_builder.create<emitc::CallOp>(callOp_builder.getUnknownLoc(), funcOp, ArrayRef<Value>{op->getOperand(0), op->getOperand(1)}); // creates the callOp
-        
-
-          replaceFunctionUses(op, callOp);
-          op->erase();
-          ++cur_func_num;
-          return true;
+          return false; // pings that no transformation occured
         }
         else 
         {
-          check_ops_regions(context, region, block, op);
-          return false;
-        }
-      }
-
-      // iterates on all regions in the operation
-      void check_ops_regions(MLIRContext* context, Region* region, Block* block, Operation* op)
-      {
-        if (op != nullptr)
-        {
-          for (Region &region_it: op->getRegions())
+          if (dyn_cast<emitc::DivOp>(operation_ptr)) // if it is 'emitc.div' operation
           {
-            check_regions(context, &region_it, block);
+            transform(context_ptr, operation_ptr);
+            return true; // pings that transformation occured
+          }
+          else
+          {
+            check_ops_regions(context_ptr, region_ptr, block_ptr, operation_ptr);
+            return false; // pings that no transformation occured
           }
         }
-        else 
+      }
+
+      // 
+      void transform(MLIRContext* context_ptr, Operation* operation_ptr)
+      {
+        Region& top_level_region = module_ptr->getRegion(0); // getting the first region in the module
+        OpBuilder wrapper_builder(top_level_region); // setting the builder for that region (ALWAYS!!! to the start of the region)
+
+        std::vector<Type> return_types  = getReturnTypes(operation_ptr); // getting string like 'i32'
+        std::vector<Type> operand_types = getOperandTypes(operation_ptr); // getting string like 'i32'
+
+        SmallVector<Type> argTypes; // for types built by builder
+        SmallVector<Type> retTypes; // for types built by builder
+        build_op_types(argTypes, operand_types, wrapper_builder); // setting types of args from strings
+        build_op_types(retTypes, return_types, wrapper_builder); // setting types of ret from strings
+
+        FuncOp funcOp = create_div_wrapper(context_ptr, wrapper_builder, argTypes, retTypes); // creates the emitc.div wrapper
+
+        OpBuilder callOp_builder(operation_ptr);
+        Operation* callOp = callOp_builder.create<emitc::CallOp>(callOp_builder.getUnknownLoc(), funcOp, 
+        ArrayRef<Value>{operation_ptr->getOperand(0), operation_ptr->getOperand(1)}); // creates the callOp
+      
+
+        replace_div_uses(operation_ptr, callOp); // replacing all uses of 'emitc.div' to the 'emitc.call'
+        operation_ptr->erase();
+        ++cur_func_num;
+      }
+
+      /**
+       * This method iterates over all nested regions that operation has.
+      */
+      void check_ops_regions(MLIRContext* context_ptr, Region* region_ptr, Block* block_ptr, Operation* operation_ptr)
+      {
+        if (operation_ptr != nullptr)
         {
-          llvm::outs() << "op is nullptr" << op;
+          for (Region &region_it: operation_ptr->getRegions())
+          {
+            check_regions(context_ptr, &region_it, block_ptr);
+          }
         }
       }
 
-      // iterates on all blocks in the region
-      void check_regions(MLIRContext* context, Region* region, Block* block)
+      /**
+       * This method iterates over all nested regions that the operation has.
+       * It has inner logic to make multiple passes inside the block to
+       * transform all 'emitc.div' operations to 'emitc.call' operations.
+      */
+      void check_regions(MLIRContext* context_ptr, Region* region_ptr, Block* block_ptr)
       {
-        if (region != nullptr)
+        if (region_ptr != nullptr)
         {
-          auto itBegin = region->getBlocks().begin();
-          auto itEnd = region->getBlocks().end();
+          auto itBegin = region_ptr->getBlocks().begin();
+          auto itEnd = region_ptr->getBlocks().end();
 
           for (;itBegin != itEnd;)
           {
-            if(check_blocks(context, region, &(*itBegin)))
-            {
-              itBegin = region->getBlocks().begin();
-              itEnd = region->getBlocks().end();
-            }
-            else 
+            if(!check_blocks(context_ptr, region_ptr, &(*itBegin)))
             {
               ++itBegin;
             }
           }
         }
-        else 
-        {
-          llvm::outs() << "region is nullptr" << region;
-        }
       }
 
-      // iterates on all operation in the block
-      bool check_blocks(MLIRContext* context, Region* region, Block* block)
+      /**
+       * This method iterates over all nested operations that block has.
+      */
+      bool check_blocks(MLIRContext* context_ptr, Region* region_ptr, Block* block_ptr)
       {
-        if (block != nullptr)
+        if (block_ptr != nullptr)
         {
-          for (Operation &op_it: block->getOperations())
+          for (Operation &op_it: block_ptr->getOperations())
           {
-            if(check_operation(context, region, block, &op_it))
+            if(check_operation(context_ptr, region_ptr, block_ptr, &op_it)) // if some operation in the block was transformed
             {
               return true;
             }
@@ -149,75 +154,56 @@ namespace
         }
         else 
         {
-          llvm::outs() << "block is nullptr" << block;
           return false;
         }
       }
 
-      void print_op_in_block(Block* block)
-      {
-        llvm::outs() << "=============================================================\n";
-        for (Operation &op_it: block->getOperations())  
-        {
-          llvm::outs() << op_it << "\n";
-        }
-        llvm::outs() << "=============================================================\n";
-      }
-
-      size_t countBlocksInRegion(Region* region) 
-      {
-        size_t count = 0;
-        for (Block &block : region->getBlocks()) 
-        {
-            count++;
-        }
-        return count;
-      }
-
-      // return the vector for returns' types
-      std::vector<Type> getReturnTypes(Operation* op) 
+      /**
+       * This method gets the results' types of operation 
+       * and stores them in the std::vector<Type>.
+      */
+      std::vector<Type> getReturnTypes(Operation* const operation_ptr) 
       {
           std::vector<Type> returnTypes;
-          for (Type resultType: op->getResultTypes()) 
+          for (Type resultType: operation_ptr->getResultTypes()) 
           {
             returnTypes.push_back(resultType);
           }
           return returnTypes;
       }
 
-      // return the vector for operands' types
-      std::vector<Type> getOperandTypes(Operation* op) 
+      /**
+       * This method gets the operands' types of operation 
+       * and stores them in the std::vector<Type>.
+      */
+      std::vector<Type> getOperandTypes(Operation* const operation_ptr)
       {
           std::vector<Type> operandTypes;
-          for (Type operandType: op->getOperandTypes()) 
+          for (Type operandType: operation_ptr->getOperandTypes()) 
           {
             operandTypes.push_back(operandType);
           }
           return operandTypes;
       }
 
-      void replaceFunctionUses(Operation* oldFunc, Operation* newFunc) 
+      /**
+       * This method replaces all uses of 'emitc.div' return value
+       * to the return value of 'emitc.call' operation.
+      */
+      void replace_div_uses(Operation* const divOp, Operation* const callOp)
       {
-        Value op_ret_val = oldFunc->getResult(0);
-        Value callOp_ret_val = newFunc->getResult(0);
-        for (auto &use : oldFunc->getUses()) 
+        Value divOp_ret_val  = divOp->getResult(0);  // getting the result of divOp as value
+        Value callOp_ret_val = callOp->getResult(0); // getting the result of callOp as value
+        
+        // iterates over all uses of divOp_ret_val and replaces them with callOp_ret_val.
+        for (auto &use : divOp->getUses()) 
         {
-            use.getOwner()->replaceUsesOfWith(op_ret_val, callOp_ret_val);
+          use.getOwner()->replaceUsesOfWith(divOp_ret_val, callOp_ret_val); 
         }
       }
 
-      void replace_div_by_call(Operation* op, Operation* callOp)
-      {
-          Value op_ret_val = op->getResult(0);
-          Value callOp_ret_val = callOp->getResult(0);
-          for (Operation *userOp : op_ret_val.getUsers()) 
-          {
-            userOp->replaceUsesOfWith(op_ret_val, callOp_ret_val);
-          }
-      }
-
-      // creates the wrapped emitc.div func
-      FuncOp create_div_wrapper(MLIRContext* context, OpBuilder& builder, SmallVector<Type>& argTypes, SmallVector<Type>& retTypes)
+      // 
+      FuncOp create_div_wrapper(MLIRContext* context_ptr, OpBuilder& builder, SmallVector<Type>& argTypes, SmallVector<Type>& retTypes)
       {    
         FunctionType funcType = builder.getFunctionType({argTypes}, {retTypes}); 
         FuncOp funcOp = builder.create<FuncOp>(builder.getUnknownLoc(), func_name + std::to_string(cur_func_num), funcType);
@@ -234,18 +220,12 @@ namespace
         return funcOp;
       }
 
-      // prints the types of returms/operand values (DEBUG ONLY)  
-      void print_types(std::vector<Type>& op_types)
+      /**
+       * This method gets the lenght in bits for the specified operands' types.
+      */
+      size_t get_bit_length(const std::vector<Type>& operand_types, const size_t operand_index)
       {
-        for (Type op_type : op_types) 
-        {
-          llvm::outs() << "Type: " << op_type << "\n";
-        }
-      }
-
-      // gets the lenght of the int/float (32-bit, 64-bit or etc.)
-      int get_bit_length(std::vector<Type>& operand_types, size_t operand_index)
-      {
+        // This part is for IntegerType bit lenght
         for (size_t j = 8; j <= 64; j *= 2)
         {
           if (operand_types[operand_index].isa<IntegerType>() && operand_types[operand_index].cast<IntegerType>().getWidth() == j)
@@ -254,6 +234,7 @@ namespace
           }
         }
 
+        // This part is for FloatType bit lenght
         for (size_t j = 32; j <= 64; j *= 2)
         {
           if (operand_types[operand_index].isa<FloatType>() && operand_types[operand_index].cast<FloatType>().getWidth() == j)
@@ -261,98 +242,81 @@ namespace
             return j;
           }
         }
-        // return error_code::OPAQUE_VAL;
       }
 
-      // creates the types of operands using builder.get...()
+      /**
+       * This method creates the types of operands using builder.get.....()
+       * For example builder.getF32Type(), etc. .
+      */
       void build_op_types(SmallVector<Type>& dest_vec, std::vector<Type>& src_vec, OpBuilder& builder)
       {
         for (size_t i = 0; i < src_vec.size(); ++i)
         {
-          switch (get_operand_type(src_vec, i))
+          switch (get_type(src_vec, i))
           {
-          case op_types::INT:
-            dest_vec.push_back(builder.getIntegerType(get_bit_length(src_vec, i)));
-            break;
-          case op_types::FLOAT:
-            if (get_bit_length(src_vec, i) == 32)
+          case operand_types::INT:
             {
-              dest_vec.push_back(builder.getF32Type());
+              dest_vec.push_back(builder.getIntegerType(get_bit_length(src_vec, i)));
+              break;    
             }
-            else 
+          case operand_types::FLOAT:
             {
-              dest_vec.push_back(builder.getF64Type());
+              if (get_bit_length(src_vec, i) == 32)
+              {
+                dest_vec.push_back(builder.getF32Type());
+              }
+              else 
+              {
+                dest_vec.push_back(builder.getF64Type());
+              }
+              break;
             }
-            break;
-          // case op_types::OPAQUE:
-          //   // argTypes.push_back(builder.getOpaque());
-          //   break;
           default:
-            break;
+            {
+              break;
+            }
           }
         }
       }
 
-      // gets the operands types (INT, FLOAT, OPAQUE)
-      op_types get_operand_type(std::vector<Type>& src_vec, size_t val_index)
+      /**
+       * This method gets the type of operand/return value
+       * using its index int the std::vector<Type>.
+      */
+      operand_types get_type(std::vector<Type>& src_vec, size_t val_index)
       {
-        for (size_t j = 8; j <= 64; j *= 2)
+        if (src_vec[val_index].isa<IntegerType>())
         {
-          if (src_vec[val_index].isa<IntegerType>())
-          {
-            return op_types::INT;
-          }
+          return operand_types::INT;
         }
 
-        for (size_t j = 32; j <= 64; j *= 2)
+        if (src_vec[val_index].isa<FloatType>())
         {
-          if (src_vec[val_index].isa<FloatType>())
-          {
-            return op_types::FLOAT;
-          }
+          return operand_types::FLOAT;
         }
-
-        // HERE MUST BE OPAQUE
       }
       
+      /**
+       * This method is used by MLIR to get all dependencies for the pass.
+      */
       void getDependentDialects(DialectRegistry& registry) const override 
       {
-        registry.insert<emitc::EmitCDialect>();
-      }
-
-      // prints the debugging info to the treminal (DEBUG ONLY)  
-      void print_debug_data()
-      {
-        llvm::raw_ostream &outs = llvm::outs(); // the ref to the terminal output 
-
-        outs << "\n=================================DEBUG=================================\n";
-        outs << "The total number of created funcs = " << cur_func_num << "\n";
-        outs << "=================================DEBUG=================================\n";
-      }
-
-      // prints op's names (DEBUG ONLY)  
-      void print_op_names(Operation* const op)  
-      {
-        static size_t op_number = 0;
-        llvm::outs () << "=========================OP_" << op_number << "=========================\n";
-        llvm::outs () << "Operation name: " << op->getName() << "\n";  
-        llvm::outs () << "=========================OP_" << op_number << "=========================\n\n";
-        ++op_number;
+        registry.insert<emitc::EmitCDialect>(); // this pass depends on to the EmitC dialect
       }
 
     // private variables section
     private:
-      std::string func_name    = "wrapped_div_func";
-      Operation* module_ptr    = nullptr;
-      Region* reg_ptr          = nullptr;
-      size_t total_block_num  = 0;
-      size_t cur_changed_block  = 0; 
-      size_t cur_func_num      = 0;    // stores total number of created function in order to create lables
+      std::string func_name    = "wrapped_div_func"; // stores the name of the wrapping fucntion  
+      Operation* module_ptr    = nullptr;            // stores the pointer to the builtin.module  
+      size_t cur_func_num      = 0;                  // stores the total number of created functions in order to create labels
   }; 
-} // namespace
+} // <--------- namespace's end
 
-// Creates an instance of the C-style expressions forming pass.
-// Exposes this pass to the outside world.
+
+/**
+ * Creates an instance of the C-style expressions forming pass.
+ * Exposes this pass to the outside world.
+*/
 std::unique_ptr<Pass> emitc::createDivToOuterFuncPass() {
   return std::make_unique<DivToOuterFuncPass>();
 }
